c5b238786eda851ceb612089345cf9a6d62272a9
chuckcastle <carlos.s.castillo@me.com>
2014-01-22 22:19:38 -0800

    local environment config changes

--- inc/connect.php
+++ inc/connect.php
@@ -5,7 +5,12 @@ if(!defined('INCLUDE_CHECK')) die('You are not allowed to execute this file dire
 
 /* Database config */
 
-$db_host		= 'kriegerdb.db.10424383.hostedresource.com';
+if($_SERVER['SERVER_NAME']=='localhost'){
+    $db_host    = 'localhost';
+} else {
+    $db_host    = 'kriegerdb.db.10424383.hostedresource.com';
+}
+
 $db_user		= 'kriegerdb';
 $db_pass		= 'kr13G3R@@';
 $db_database	= 'kriegerdb'; 
